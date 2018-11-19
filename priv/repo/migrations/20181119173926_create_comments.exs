d4f77bdebdaac184866c622fbdc62a484dd32096
Mark Del Rosario <MarkESDR@gmail.com>
2018-11-19 12:40:40 -0500

    Add comments

---
+++ priv/repo/migrations/20181119173926_create_comments.exs
@@ -0,0 +1,12 @@
+defmodule JsonapiIssue134.Repo.Migrations.CreateComments do
+  use Ecto.Migration
+
+  def change do
+    create table(:comments) do
+      add :body, :string
+
+      timestamps()
+    end
+
+  end
+end
